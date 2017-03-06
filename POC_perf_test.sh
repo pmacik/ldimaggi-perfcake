db5f598294526694e97b2565e9cee201fbbc7dc7
Len DiMaggio <ldimaggi@redhat.com>
2017-03-06 09:29:09 -0500

    Add files via upload

--- POC_perf_test.sh
+++ POC_perf_test.sh
@@ -75,7 +75,11 @@ echo $token
 # Insert the token into the Perfcake configuration file
 sed -e "s/THETOKEN/$token/g" $PERFCAKE_HOME/resources/scenarios/input.xml > $PERFCAKE_HOME/resources/scenarios/output.xml
 
-# Run the test */
+echo "======= prefcake config file ================"
+cat $PERFCAKE_HOME/resources/scenarios/output.xml
+echo "======= prefcake config file ================"
+
+# Run the test - single token == single user */
 $PERFCAKE_HOME/bin/perfcake.sh -s output.xml -Dthread.count=10 
 
 # Query for the workitems
