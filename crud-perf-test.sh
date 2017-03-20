1a26276cf6be66e61139ae26d22ae80cb52df4b0
Pavel Macík <pmacik@redhat.com>
2017-03-20 15:15:21 +0100

    [CRUD PoC] Updated for fixed PerfCake

--- crud-perf-test.sh
+++ crud-perf-test.sh
@@ -18,17 +18,20 @@ export WORK_ITEM_IDS=$PERFORMANCE_RESULTS/workitem-id.list
 export POC_RESULTS=$PERFORMANCE_RESULTS/poc-results.log
 
 # Get Perfcake, and our preconfigured Perfcake test config file
-git clone -b feature/#379-workaround  https://github.com/PerfCake/PerfCake PerfCake.git
+git clone -b devel  https://github.com/PerfCake/PerfCake PerfCake.git
+cd PerfCake.git
+git checkout c887baaa13b640dc83ef7203f79d8c4818512aa4
+cd ..
 mvn -f PerfCake.git/pom.xml clean package assembly:single -DskipTests
+rm -rf $PERFCAKE_HOME
 unzip PerfCake.git/perfcake/target/perfcake-$PERFCAKE_VERSION-bin.zip
 rm -rvf PerfCake.git
 cp create.xml $PERFCAKE_HOME/resources/scenarios/
 cp read.xml $PERFCAKE_HOME/resources/scenarios/
+cp update.xml $PERFCAKE_HOME/resources/scenarios/
 cp delete.xml $PERFCAKE_HOME/resources/scenarios/
 
 # Run the test (prepare clean env)
-rm -rf $WORK_ITEM_IDS
-rm -rf $TOKEN_LIST
 rm -rf $PERFORMANCE_RESULTS
 mkdir -p $PERFORMANCE_RESULTS
 
@@ -56,7 +59,9 @@ curl -silent -X GET --header 'Accept: application/json' 'http://'$SERVER_HOST':'
 $PERFCAKE_HOME/bin/perfcake.sh -s create $PERFCAKE_PROPS
 cat $PERFCAKE_HOME/perfcake-validation.log | grep Response | sed -e 's,.*/api/workitems/\([^"/]*\)/.*".*,\1,g' > $WORK_ITEM_IDS
 cat $PERFCAKE_HOME/create-average-throughput.csv
-mv $PERFCAKE_HOME/perfcake-validation.log $PERFORMANCE_RESULTS/perfcake-validation-create.log
+mv $PERFCAKE_HOME/create-average-throughput.csv $PERFORMANCE_RESULTS
+#mv $PERFCAKE_HOME/perfcake-validation.log $PERFORMANCE_RESULTS/perfcake-validation-create.log
+rm -vf $PERFCAKE_HOME/perfcake-validation.log
 mv $PERFCAKE_HOME/perfcake.log $PERFORMANCE_RESULTS/perfcake-create.log
 
 echo "After CREATE:" >> $POC_RESULTS
@@ -65,23 +70,33 @@ curl -silent -X GET --header 'Accept: application/json' 'http://'$SERVER_HOST':'
 # C(R)UD
 $PERFCAKE_HOME/bin/perfcake.sh -s read $PERFCAKE_PROPS
 cat $PERFCAKE_HOME/read-average-throughput.csv
-mv $PERFCAKE_HOME/perfcake-validation.log $PERFORMANCE_RESULTS/perfcake-validation-read.log
+mv $PERFCAKE_HOME/read-average-throughput.csv $PERFORMANCE_RESULTS
+#mv $PER	FCAKE_HOME/perfcake-validation.log $PERFORMANCE_RESULTS/perfcake-validation-read.log
+rm -vf $PERFCAKE_HOME/perfcake-validation.log
 mv $PERFCAKE_HOME/perfcake.log $PERFORMANCE_RESULTS/perfcake-read.log
 
 echo "After READ:" >> $POC_RESULTS
 curl -silent -X GET --header 'Accept: application/json' 'http://'$SERVER_HOST':'$SERVER_PORT'/api/workitems' |  sed s/.*totalCount/\\n\\n\\n"totalCount of workitems in DB"/g | sed s/\"//g | sed s/}//g| grep totalCount >> $POC_RESULTS
 
 # CR(U)D
-# TODO: Coming soon...
+#TODO: Coming soon...
+#$PERFCAKE_HOME/bin/perfcake.sh -s update $PERFCAKE_PROPS
+#cat $PERFCAKE_HOME/update-average-throughput.csv
+#mv $PERFCAKE_HOME/update-average-throughput.csv $PERFORMANCE_RESULTS
+#mv $PERFCAKE_HOME/perfcake-validation.log $PERFORMANCE_RESULTS/perfcake-validation-update.log
+#rm -vf $PERFCAKE_HOME/perfcake-validation.log
+#mv $PERFCAKE_HOME/perfcake.log $PERFORMANCE_RESULTS/perfcake-update.log
 
 echo "After UPDATE:" >> $POC_RESULTS
 curl -silent -X GET --header 'Accept: application/json' 'http://'$SERVER_HOST':'$SERVER_PORT'/api/workitems' |  sed s/.*totalCount/\\n\\n\\n"totalCount of workitems in DB"/g | sed s/\"//g | sed s/}//g| grep totalCount >> $POC_RESULTS
 
 # CRU(D)
 $PERFCAKE_HOME/bin/perfcake.sh -s delete $PERFCAKE_PROPS
-cat $PERFCAKE_HOME/read-average-throughput.csv
-mv $PERFCAKE_HOME/perfcake-validation.log $PERFORMANCE_RESULTS/perfcake-validation-read.log
-mv $PERFCAKE_HOME/perfcake.log $PERFORMANCE_RESULTS/perfcake-read.log
+cat $PERFCAKE_HOME/delete-average-throughput.csv
+mv $PERFCAKE_HOME/delete-average-throughput.csv $PERFORMANCE_RESULTS
+#mv $PERFCAKE_HOME/perfcake-validation.log $PERFORMANCE_RESULTS/perfcake-validation-delete.log
+rm -vf $PERFCAKE_HOME/perfcake-validation.log
+mv $PERFCAKE_HOME/perfcake.log $PERFORMANCE_RESULTS/perfcake-delete.log
 
 echo "After DELETE:" >> $POC_RESULTS
 curl -silent -X GET --header 'Accept: application/json' 'http://'$SERVER_HOST':'$SERVER_PORT'/api/workitems' |  sed s/.*totalCount/\\n\\n\\n"totalCount of workitems in DB"/g | sed s/\"//g | sed s/}//g| grep totalCount >> $POC_RESULTS
